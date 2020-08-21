b9ce2946c6f6b484a2cad71103c3e0e06cdc731a
harsh957 <developedbyharsh@gmail.com>
2020-08-22 02:53:53 +0530

    inital commit adding boot project

---
+++ morseit-messaging/src/main/java/com/morseit/messaging/MorseitMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.morseit.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorseitMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorseitMessagingApplication.class, args);
+	}
+
+}
---
+++ morseit-messaging/src/test/java/com/morseit/messaging/MorseitMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morseit.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorseitMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
